ae7d49a973d9e245408110f2464717acfa5ae66c
balduin05 <balduingiovanni05@gmail.com>
2023-11-17 10:49:16 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // ARRAYLIST:
+
+        ArrayList<String> aL = new ArrayList<>();
+
+        System.out.println(" - ARRAY LIST - ");
+        System.out.println();
+        System.out.println("Size: " + aL.size());
+
+        aL.add("ciao");
+        System.out.println(aL);
+        System.out.println("Size: " + aL.size());
+        System.out.println();
+
+        aL.add("Buonasera");
+        System.out.println(aL);
+        System.out.println("Size: " + aL.size());
+        System.out.println();
+
+        aL.remove(0);
+        System.out.println(aL);
+        System.out.println("Size: " + aL.size());
+
+        System.out.println(" ----------------------------- ");
+
+        // VECTOR:
+
+        Vector<String> v = new Vector<>(5, 1);
+
+        System.out.println(" - VECTOR - ");
+        System.out.println("Capacity: " + v.capacity());
+        System.out.println("Size: " + v.size());
+        System.out.println();
+
+        v.add("Ciao");
+        System.out.println(v);
+        System.out.println("Capacity: " + v.capacity());
+        System.out.println("Size: " + v.size());
+        System.out.println();
+
+        v.add("Notte");
+        System.out.println(v);
+        System.out.println("Capacity: " + v.capacity());
+        System.out.println("Size: " + v.size());
+        System.out.println();
+
+        v.add("Notte");
+        v.add("Notte");
+        v.add("Notte");
+        v.add("Notte");
+        v.add("Notte");
+        v.add("Notte");
+        v.add("Notte");
+        v.add("Notte");
+        System.out.println(v);
+        System.out.println("Capacity: " + v.capacity());
+        System.out.println("Size: " + v.size());
+        System.out.println();
+
+        v.add("Giorno");
+        System.out.println(v);
+        System.out.println("Capacity: " + v.capacity());
+        System.out.println("Size: " + v.size());
+
+
+    }
+}
